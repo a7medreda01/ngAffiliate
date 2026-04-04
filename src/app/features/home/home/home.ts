8ad440fc6129cec663a7e5205701af350be37852
a7medreda01 <a7med.reda01@gmail.com>
2026-04-05 00:57:54 +0200

    update footer design add products

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../../services/product/product-service';
+import { Product } from '../../../models/product-model';
+import { FourProduct } from '../../../shared/components/four-product/four-product';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FourProduct],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+constructor(private productService:ProductService){
+  this.products=this.productService.products
+}
+products!:Product[];
 
 }
